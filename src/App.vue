611d8712cb498035c7e5fc58281a83cc5c5b94f1
Abah Gideon <abahgideon111@gmail.com>
2024-11-13 16:43:58 +0100

    update to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Navbar from '@/components/navbar/NavbarComponent.vue'
+import Navbar from '@/components/Navbar/NavbarComponent.vue'
 import FooterSection from '@/components/FooterSection/FooterComponent.vue'
 </script>
 
